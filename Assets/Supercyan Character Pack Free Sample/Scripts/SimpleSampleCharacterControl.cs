6dcad93343f9f939ee7bba3b998d42ec3932ff09
Jenni <jfelton@csu.fullerton.edu>
2020-08-18 13:47:45 -0700

    Deleted camera that was on character, added Pool script

--- Assets/Supercyan Character Pack Free Sample/Scripts/SimpleSampleCharacterControl.cs
+++ Assets/Supercyan Character Pack Free Sample/Scripts/SimpleSampleCharacterControl.cs
@@ -138,7 +138,7 @@ public class SimpleSampleCharacterControl : MonoBehaviour {
         float v = Input.GetAxis("Vertical");
         float h = Input.GetAxis("Horizontal");
 
-        Transform camera = Camera.main.transform;
+       // Transform camera = Camera.main.transform;
 
         if (Input.GetKey(KeyCode.LeftShift)) {
             v *= m_walkScale;
@@ -148,20 +148,19 @@ public class SimpleSampleCharacterControl : MonoBehaviour {
         m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
         m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
 
-        Vector3 direction = camera.forward * m_currentV + camera.right * m_currentH;
+        //Vector3 direction = camera.forward * m_currentV + camera.right * m_currentH;
 
-        float directionLength = direction.magnitude;
-        direction.y = 0;
-        direction = direction.normalized * directionLength;
+      //  float directionLength = direction.magnitude;
+      //  direction.y = 0;
+      //  direction = direction.normalized * directionLength;
 
-        if (direction != Vector3.zero) {
-            m_currentDirection = Vector3.Slerp(m_currentDirection, direction, Time.deltaTime * m_interpolation);
+      //  if (direction != Vector3.zero) {
+      //      m_currentDirection = Vector3.Slerp(m_currentDirection, direction, Time.deltaTime * m_interpolation);
+       //     transform.rotation = Quaternion.LookRotation(m_currentDirection);
+        //    transform.position += m_currentDirection * m_moveSpeed * Time.deltaTime;
 
-            transform.rotation = Quaternion.LookRotation(m_currentDirection);
-            transform.position += m_currentDirection * m_moveSpeed * Time.deltaTime;
-
-            m_animator.SetFloat("MoveSpeed", direction.magnitude);
-        }
+        //    m_animator.SetFloat("MoveSpeed", direction.magnitude);
+       // }
 
         JumpingAndLanding();
     }
