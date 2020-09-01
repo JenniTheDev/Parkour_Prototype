7f986420239f43d4d10bd3290a90a6f73ea26c2e
Vast <Vastlee@gmail.com>
2020-09-01 05:40:51 -0700

    Added KillZone, Setup Killzone Layers

---
+++ Assets/Scripts/KillZone.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    [SerializeField] private LayerMask killLayers;
+
+    private void OnTriggerEnter(Collider other) {
+        if((killLayers & (1 << other.gameObject.layer)) != 0) {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
